973f6651d2729289e03ef9868ac44c64d197e02c
M1F1 <filek.michal@gmail.com>
2019-01-16 12:51:30 +0100

    adding test that will fail

--- test/test_unit/test_functions.py
+++ test/test_unit/test_functions.py
@@ -13,6 +13,9 @@ class TestCalculator(unittest.TestCase):
     def test_subtraction(self):
         assert 2 == main.subtract(4, 2)
 
+    def test_multiplication(self):
+        assert 100 == main.multiply(10, 10)
+
 
 if __name__ == "__main__":
     unittest.main()
