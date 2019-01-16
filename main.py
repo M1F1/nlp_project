a299e498974e7a7ae6aa3b61492340bf414bc293
M1F1 <filek.michal@gmail.com>
2019-01-16 11:43:41 +0100

    adding functions

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-import numpy as np
-import pandas as pd
-
-
 def tst():
     return 1
 
-if __name__ == '__main__':
+
+"""
+Calculator library containing basic math operations.
+"""
+
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+
+def subtract(first_term, second_term):
+    return first_term - second_term
+
+
+if __name__ == "__main__":
     tst()
